b8a73a3f1bdefe372e10970702c2b40b88c5c598
Martin Ziegler <martin.ziegler3398@gmail.com>
2023-12-09 01:05:49 +0900

    Migrated to memories-v1

--- berealdownloader/api/memories.py
+++ berealdownloader/api/memories.py
@@ -2,22 +2,22 @@ import requests
 
 
 def get_memories_data(access_token: str):
-    url = "https://mobile.bereal.com/api/feeds/memories"
+    url = "https://mobile.bereal.com/api/feeds/memories-v1"
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
 
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        raise Exception("Could not get mmories")
+        raise Exception("Could not get memories")
 
     json_data = response.json()
     data = []
     for memory in json_data["data"]:
         data.append({
             "date": memory["memoryDay"],
-            "primary": memory["primary"]["url"],
-            "secondary": memory["secondary"]["url"]
+            "primary": memory["mainPostPrimaryMedia"]["url"],
+            "secondary": memory["mainPostSecondaryMedia"]["url"]
         })
 
     return data
